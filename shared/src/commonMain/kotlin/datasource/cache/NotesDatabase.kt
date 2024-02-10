69c526b80b2ba057078ed378a15b7ccfa04ca9b2
Rae-An Andres <raeandres@users.noreply.github.com>
2024-02-11 05:52:52 +0800

    Common code updates
    Added getAllNotes db operation
    added note mapping

--- shared/src/commonMain/kotlin/datasource/cache/NotesDatabase.kt
+++ shared/src/commonMain/kotlin/datasource/cache/NotesDatabase.kt
@@ -2,6 +2,7 @@ package datasource.cache
 
 import com.raeandres.kmm.cache.AppDatabase
 import datasource.DatabaseDriverFactory
+import model.Note
 
 internal class NotesDatabase(databaseDriverFactory: DatabaseDriverFactory){
     private val database = AppDatabase(databaseDriverFactory.createDriver())
@@ -12,5 +13,23 @@ internal class NotesDatabase(databaseDriverFactory: DatabaseDriverFactory){
         dbQuery.removeAllNotes()
     }
 
-    
+    internal fun getAllNotes(): List<Note> {
+        return dbQuery.selectAllNote(::noteMapping).executeAsList()
+    }
+
+    private fun noteMapping(
+         id: Long,
+         name: String,
+         description: String?,
+         createdDate: String
+    ): Note{
+        return Note(
+            id = id.toString(),
+            name = name,
+            description = description ?: "",
+            createdDate = createdDate
+        )
+    }
+
+
 }
