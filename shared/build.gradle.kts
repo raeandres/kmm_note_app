581580b47d288c78fdbf94043461c1b22c183cb3
Rae-An Andres <raeandres@users.noreply.github.com>
2024-02-11 04:49:35 +0800

    Common module updates
    Added common dependencies
    
    coroutinesVersion = "1.7.3"
    ktorVersion = "2.3.7"
    sqlDelightVersion = "1.5.5"
    dateTimeVersion = "0.4.1"

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -2,6 +2,8 @@
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidLibrary)
+    kotlin("plugin.serialization").version("1.9.22")
+    id("com.squareup.sqldelight").version("1.5.5")
 }
 
 kotlin {
@@ -24,10 +26,29 @@ kotlin {
             isStatic = true
         }
     }
+
+    // dependency versions
+    val coroutinesVersion = "1.7.3"
+    val ktorVersion = "2.3.7"
+    val sqlDelightVersion = "1.5.5"
+    val dateTimeVersion = "0.4.1"
     
     sourceSets {
         commonMain.dependencies {
-            // put your Multiplatform dependencies here
+            implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
+            implementation("io.ktor:ktor-client-core:$ktorVersion")
+            implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
+            implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
+            implementation("com.squareup.sqldelight:runtime:$sqlDelightVersion")
+            implementation("org.jetbrains.kotlinx:kotlinx-datetime:$dateTimeVersion")
+        }
+        androidMain.dependencies {
+            implementation("io.ktor:ktor-client-android:$ktorVersion")
+            implementation("com.squareup.sqldelight:android-driver:$sqlDelightVersion")
+        }
+        iosMain.dependencies {
+            implementation("io.ktor:ktor-client-darwin:$ktorVersion")
+            implementation("com.squareup.sqldelight:native-driver:$sqlDelightVersion")
         }
     }
 }
