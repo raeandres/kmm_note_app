338b134edec5451dad3c0cf0203869f223aa7f25
Rae-An Andres <raeandres@users.noreply.github.com>
2024-02-11 03:22:29 +0800

    Configuration updates
    Updated jvmTarget to 17
    updated sourceCompatibility to JavaVersion.VERSION_17

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -8,10 +8,11 @@ plugins {
 }
 
 kotlin {
+    jvmToolchain(17)
     androidTarget {
         compilations.all {
             kotlinOptions {
-                jvmTarget = "1.8"
+                jvmTarget = "17"
             }
         }
     }
@@ -60,8 +61,8 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     dependencies {
         debugImplementation(libs.compose.ui.tooling)
